e42cd6479e9d3ef0eaa15e14af7a337b6951d5ea
Surrui <Surrui@HAL9000>
2016-02-11 00:56:35 +0100

    Commit: Programa avanzado - Genera segundo frame con boton de cerrar

--- Mental/src/main/java/net/javierjimenez/App.java
+++ Mental/src/main/java/net/javierjimenez/App.java
@@ -1,18 +1,19 @@
 package net.javierjimenez;
 
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JButton;
-import java.awt.Insets;
+import javax.swing.JTextField;
 
 public class App {
 
-	private JLabel lblCalculadora = new JLabel("0");
+	private JLabel lblOperacio = new JLabel("Operacio");
 	
 	private JFrame frame;
 
@@ -50,27 +51,89 @@ public class App {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		frame.getContentPane().setLayout(gridBagLayout);
 		
-		JButton btnHola = new JButton("Hola");
-		GridBagConstraints gbc_btnHola = new GridBagConstraints();
-		gbc_btnHola.insets = new Insets(0, 0, 0, 5);
-		gbc_btnHola.gridx = 0;
-		gbc_btnHola.gridy = 0;
-		frame.getContentPane().add(btnHola, gbc_btnHola);
-		
-		lblCalculadora.setBackground(Color.gray);
-		lblCalculadora.setForeground(Color.BLACK);
-		lblCalculadora.setOpaque(true);
+		JLabel lblTitol = new JLabel("Quan dóna...");
 		GridBagConstraints g = new GridBagConstraints();
+		g.fill = GridBagConstraints.BOTH;
+		
+		g.weightx = 0.5;
+		g.weighty = 1;
+		
+		g.gridwidth = 1;
+		g.gridheight = 1;
+		
+		g.gridx = 0;
+		g.gridy = 0;
+		
+		frame.getContentPane().add(lblTitol, g);
+		g.fill = GridBagConstraints.BOTH;
+
+		g.weightx = 0.5;
+		g.weighty = 1.5;
+
+		g.gridwidth = 1;
+		g.gridheight = 1;
+
+		g.gridx = 0;
+		g.gridy = 1;
+		frame.getContentPane().add(lblOperacio, g);
+		
+		JLabel lblError = new JLabel("");
+		g.fill = GridBagConstraints.BOTH;
+		
+		g.weightx = 0.5;
+		g.weighty = 0.5;
+		
+		g.gridwidth = 1;
+		g.gridheight = 1;
+		
+		g.gridx = 0;
+		g.gridy = 2;
+		
+		frame.getContentPane().add(lblError, g);
+		
+		JTextField resposta = new JTextField("");
 		g.fill = GridBagConstraints.BOTH;
 
 		g.weightx = 0.5;
 		g.weighty = 0.5;
 
-		g.gridwidth = 3;
+		g.gridwidth = 1;
 		g.gridheight = 1;
 
-		g.gridx = 1;
-		g.gridy = 0;
-		frame.getContentPane().add(lblCalculadora, g);
+		g.gridx = 0;
+		g.gridy = 3;
+		frame.getContentPane().add(resposta, g);
+		
+		JButton buttonPnt = new JButton(".");
+		g.fill = GridBagConstraints.BOTH;
+
+		g.weightx = 0.5;
+		g.weighty = 0.5;
+
+		g.gridwidth = 1;
+		g.gridheight = 1;
+
+		g.gridx = 0;
+		g.gridy = 4;
+		frame.getContentPane().add(buttonPnt, g);
+		
+		buttonPnt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame finalFrame = new JFrame();
+				finalFrame.setVisible(true);
+				finalFrame.setBounds(150, 150, 400, 200);
+				finalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				JButton closeAll = new JButton("Aceptar");
+				finalFrame.getContentPane().add(closeAll);
+				
+				closeAll.addActionListener(new ActionListener () {
+					public void actionPerformed(ActionEvent e) {
+						frame.dispose();
+						finalFrame.dispose();
+					}
+				});
+			}
+		});
 	}
 }
